5c3584e4d0463b390db24f76631df8eee22abd59
cmahima <36598677+cmahima@users.noreply.github.com>
2018-08-01 15:04:54 -0400

    Update video.py
    Captures images on Pi

--- video.py
+++ video.py
@@ -5,6 +5,6 @@ camera=PiCamera()
 camera.resolution=(200,200)
 #camera.start_preview()
 for i in range(10):
-    sleep(0.0001)
+    sleep(0.1)
     camera.capture('/home/turtle/images/image%s.jpg' % i, resize=(640,480))
 #camera.stop_preview()
