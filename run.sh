5f555dfce3e3be7dd72a200bf05052531547f178
cmahima <36598677+cmahima@users.noreply.github.com>
2018-08-07 14:01:28 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,7 @@
 
-#set -m
-#python record.py & python call.py && fg
-#parallel ::: python record.py python call.py 
-#killall python record.py
 
-python record.py &
-P1=$!
-python call.py &
-P2=$!
-wait $P1 $P2
-killall python record.py
+python video.py & python test.py ; fg
+killall python video.py
 rm -rf /home/mahima/images/*
 #cd /home/mahima/images 
 #del *.* /F /S /Q /A: R /A: H /A: 
