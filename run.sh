1d7867e926770f0f74498f4101fc0912ef576a65
cmahima <36598677+cmahima@users.noreply.github.com>
2018-08-21 14:30:04 -0400

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,9 +1,33 @@
-cd tensor_client
+#! /bin/bash
+echo Hi... Welcome to CERAS lab
 
-python video.py & python test.py ; fg
-killall python video.py
-rm -rf /home/mahima/images/*
-#cd /home/mahima/images 
-#del *.* /F /S /Q /A: R /A: H /A: 
-#rmdir /home/mahima/images
-#mkdir /home/mahima/images
+read -p "Press T to use Tensor and W to use Watson " varname
+
+#read varname
+
+#echo Face the camera
+ 
+
+
+
+if  [[ $varname = T ]] ;
+then
+ echo Face the camera... 
+ sleep 3
+ cd tensor_client
+ python video.py #& python test.py ; fg
+ python test.py 
+ killall python video.py
+ rm -rf /home/turtle/images/*
+
+else
+ echo Face the camera...
+ sleep 3
+ cd
+ cd watson_client
+ python video.py #& python sample_script.py ; fg
+ python run.py
+ killall python video.py
+ rm -rf /home/turtle/images/*
+fi
+
